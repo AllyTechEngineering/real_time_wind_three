0438eae9201d6393a26c783f5005a6c46f1d3195
allytechllc <bob.taylor.mscs.mba@gmail.com>
2023-06-13 15:37:20 -0400

    Not sure why this file is red?

---
+++ lib/models/location_arguments.dart
@@ -0,0 +1,9 @@
+class LocationArguments {
+  final String latitudeLocation;
+  final String longitudeLocation;
+  final int resetWidgetCount;
+  final int tempSelectionValue;
+  final int speedSelectionValue;
+
+  LocationArguments(this.latitudeLocation, this.longitudeLocation, this.resetWidgetCount, this.tempSelectionValue, this.speedSelectionValue);
+}
